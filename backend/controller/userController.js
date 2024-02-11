4688e737f5521c73c67c8e0e82c1b6a9e3892cae
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-12 00:02:47 +0100

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -5,9 +5,20 @@ const createToken = (_id) => {
   return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d' })
 }
 
-//login user 
+// login a user
 const loginUser = async (req, res) => {
-  res.json({mssg: 'login user'})
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 //signup user
