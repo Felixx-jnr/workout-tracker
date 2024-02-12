bcdd363d929de9f09f01d8a8406e4f642467d62a
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-12 13:06:28 +0100

    user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //pages and components
 import Home from './pages/Home.';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -16,6 +18,18 @@ function App() {
               path='/'
               element={<Home/>}
             />
+
+            <Route
+              path='/login'
+              element={<Login/>}
+            />
+
+            <Route
+              path='/signup'
+              element={<Signup/>}
+            />
+
+
           </Routes>
 
         </div>
